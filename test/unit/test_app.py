76a11c88e9be6da048a9623bf0ae9a37b6cb3fb7
IvarCarrera <ivar.carrera@gmail.com>
2023-04-17 20:55:16 -0300

    Installing and Configuring Initial Tools

--- test/unit/test_app.py
+++ test/unit/test_app.py
@@ -2,7 +2,8 @@ from src.app import app
 
 
 def test_hello_world():
+    status_code_ok = 200
     with app.test_client() as client:
         response = client.get('/')
-        assert response.status_code == 200
+        assert response.status_code == status_code_ok
         assert response.data == b'Hello, Docker!'
