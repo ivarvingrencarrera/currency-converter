24aadb61ddf614b773684da8da67f41298a7a93f
IvarCarrera <ivar.carrera@gmail.com>
2023-04-17 21:14:20 -0300

    Installing and Configuring Initial Tools

--- test/unit/test_app.py
+++ test/unit/test_app.py
@@ -1,7 +1,7 @@
 from src.app import app
 
 
-def test_hello_world():
+def test_hello_world() -> None:
     status_code_ok = 200
     with app.test_client() as client:
         response = client.get('/')
